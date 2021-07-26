13e8a59e361986fe932edffc8830b787a66e431a
Priscilla Robinson <priscilla.wojtowicz@gmail.com>
2021-07-25 21:16:14 -0500

    git commit -m "Editing the PyBank" git commit -m "Editing the PyPoll"

--- PyBank/main.py
+++ PyBank/main.py
@@ -3,6 +3,7 @@ import csv, os
 # Path to collect data from Resources Folder
 PyBank_csv = os.path.join("Resources", "budget_data.csv")
 
+# Define variables
 total = 0
 months = 0
 preBudget = 0
@@ -13,7 +14,7 @@ gDecrease = 0
 gDecreaseDate = ""
 
 
-#Open the CSV file
+#Open the CSV file and skip header
 with open (PyBank_csv) as csv_file:
     PyBank_reader = csv.reader(csv_file, delimiter = ",")
     next(PyBank_reader)
@@ -22,41 +23,36 @@ with open (PyBank_csv) as csv_file:
     changeCount = 0
    
     for row in PyBank_reader:
-
+        # Calculate total months and revenue
         months += 1
         currentBudget = int(row[1])
-
+        # Calculate total
         total += currentBudget
-
+        # Calculate the changes in "Profit/Losses" 
         if preBudget != 0:
             change = currentBudget - preBudget
             changeTotal += change
             changeCount += 1
 
         preBudget = currentBudget    
-
+        # Greatest increase in profits (date and amount) over entire period
         if change > gIncrease:
             gIncrease = change
             gIncreaseDate = row[0]
-
+        # Greatest decrease in profits (date and amount) over the entire period
         if change < gDecrease:
             gDecrease = change
             gDecreaseDate = row[0] 
+# Calculate average revenue change
+    average_change = changeTotal / changeCount
 
-        average_change = sum({gIncrease} + {gDecrease}/ {months})  
 
-print()
-print (f'Financial Analysis')
-print(f'----------------------------')
-print(f'Total Months: {months}')
-print(f'Total: ${total:,}')
-print(f'Average Change:{average_change}')
 output = f"""
 Financial Analysis
 ----------------------------
 Total Months: {months}
 Total: ${total:,}
-Average  Change: $-2315.12
+Average  Change: ${average_change:.2f}
 Greatest Increase in Profits: {gIncreaseDate} (${gIncrease:,})
 Greatest Decrease in Profits: {gDecreaseDate} (${gDecrease:,})
 """
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -1,53 +1,66 @@
 import csv, os
 
+
 # Path to collect data from Resources Folder
 PyPoll_csv = os.path.join("Resources", "election_data.csv")
 
-#set variables
-votes = 0
-
-#candidate list
+# Define variables
 candidate_list = []
 candidate_votes = {}
+total_votes = 0
+maxVotes = 0
+winner = ""
 
-
-
-#Open the CSV file
+# Open the CSV file
 with open (PyPoll_csv) as csv_file:
     PyPoll_reader = csv.reader(csv_file, delimiter = ",")
     next(PyPoll_reader)
 
     for row in PyPoll_reader:
-        votes += 1
-        
-        #read in candidate from column 3
+        # Total number of votes
+        total_votes += 1
+        # Get candidate name
         candidate_name = row[2]
-                
-        #if statement for unique candidate using append
+
+        # Count votes for each candidate
         if candidate_name not in candidate_list:
             candidate_list.append(candidate_name)
             candidate_votes[candidate_name] = 0
-        
-        candidate_votes[candidate_name] += 1
-        vote_perentage = (candidate_votes/ votes) * 100
+
+        candidate_votes[candidate_name] += 1     
+
+# print(candidate_votes)  
+
+ # For loop to find winner of election
+for candidate, votes in candidate_votes.items():
+    if maxVotes < votes:
+            # found candidate with more votes, update current name
+            maxVotes = votes
+            winner = candidate
 
 
+print(f'\n{winner}  {maxVotes}\n')
+#percentage of votes each candidate won
+#total number of votes each candidate won
+#winner of the election based on popular vote
 
 output = f"""
 Election Results
 -------------------------
-Total: {votes:,}
-vote percentage: {vote_perentage}
+Total Votes: {total_votes}
 -------------------------
-[{candidate_list}:{candidate_votes}]
-Khan: 63.000% (2218231)
-Correy: 20.000% (704200)
-Li: 14.000% (492940)
-O'Tooley: 3.000% (105630)
+Khan: {candidate_votes["Khan"] / total_votes * 100:.3f}% ({candidate_votes["Khan"]})
+Correy: {candidate_votes["Correy"] / total_votes * 100:.3f}% ({candidate_votes["Correy"]})
+Li: {candidate_votes["Li"] / total_votes * 100:.3f}% ({candidate_votes["Li"]})
+O'Tooley: {candidate_votes["O'Tooley"] / total_votes * 100:.3f}% ({candidate_votes["O'Tooley"]})
 -------------------------
-Winner: Khan
+Winner: {winner}
 -------------------------
 """
 
 print(output)
+
+with open ("analysis/poll.txt", 'w') as output_file:
+    output_file.write(output)
+
 
